e23c11ebe131421d9fa3d966b88b72bc1dadcbe6
snixie <fsd.rabgay@gmail.com>
2023-03-27 17:15:13 +0600

    updates

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Key": "adef80813dmsh3f7f3c7f78f65b2p111c9ejsn08c7d9daf0df",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
@@ -18,15 +18,19 @@ const useFetch = (endpoint, query) => {
 
   const fetchData = async () => {
     setIsLoading(true);
-
     try {
       const response = await axios.request(options);
-
       setData(response.data.data);
       setIsLoading(false);
     } catch (error) {
-      setError(error);
-      console.log(error)
+      if (error.response.status === 429) {
+        // Add a delay of 2 second before retrying the request
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        fetchData();
+      } else {
+        setError(error);
+        alert("There is an error");
+      }
     } finally {
       setIsLoading(false);
     }
